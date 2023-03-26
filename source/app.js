7b9519f996eff04165a645359b03debdc6ef77ad
Kto-to015 <118625333+Kto-to015@users.noreply.github.com>
2023-03-26 18:32:00 +0500

    News

--- source/app.js
+++ source/app.js
@@ -1,44 +1,58 @@
 const list = document.querySelector('.list');
 
-let tasks = [
-  {
-    title: 'Задача 1',
-    done: true
-  },
-  {
-    title: 'Задача 2',
-    done: false
-  },
-  {
-    title: 'Задача 3',
-    done: false
-  }
-];
+let tasks = [];
 
-document.getElementById('count'). textContent = 'Количество задач: ' + tasks.length
+if(localStorage.getItem('tasks')) {
+  tasks = JSON.parse(localStorage.getItem('tasks'));
+}
+
+// let tasks = [
+//    {
+//      title: 'Задача 1',
+//      done: false
+//    },
+//    {
+//      title: 'Задача 2',
+//      done: false
+//    },
+//    {
+//      title: 'Задача 3',
+//      done: false
+//    }
+// ];
+
 
 render();
 
 function render() {
   clear();
-  tasks.forEach(task => {
+
+  tasks.forEach((task, index) => {
     let _element = document.createElement('div');
     _element.classList.add('element');
   
     let _input = document.createElement('input');
     _input.setAttribute('id', 'done');
     _input.setAttribute('type', 'checkbox');
+    _input.setAttribute('onclick', 'onChecked(' + index + ');');
+    _input.checked = task.done;
   
     let _paragraph = document.createElement('p');
     _paragraph.classList.add('text');
     _paragraph.textContent = task.title;
+    if(task.done) { _paragraph.classList.add('checked') }
+
+    let _button = document.createElement('button');
+    _button.textContent = 'Удалить';
+    _button.setAttribute('onclick', 'onDelete(' + index + ');');
   
     _element.appendChild(_input);
     _element.appendChild(_paragraph);
+    _element.appendChild(_button);
     
     list.appendChild(_element);
-  
-    console.log(_element);
+
+    document.getElementById('count'). textContent = 'Количество задач: ' + tasks.length
   }); 
 }
 
@@ -57,7 +71,31 @@ function add() {
   }
   tasks.push(_element);
 
+  save();
   render();
 
   _input.value = '';
+}
+
+function onChecked(index) {
+  if(tasks[index].done) {
+    tasks[index].done = false;
+  } else {
+    tasks[index].done = true;
+  }
+
+  save();
+  render();
+}
+
+function onDelete(index) {
+  // delete tasks[index];
+  tasks.splice(index, 1);
+
+  save();
+  render();
+}
+
+function save() {
+  localStorage.setItem('tasks', JSON.stringify(tasks));
 }
